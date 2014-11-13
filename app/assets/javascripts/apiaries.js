ad735bb0fb7236a0c00539a5d26761badba50649
Aleksandar Popovic <web.development.serbia@gmail.com>
2014-11-13 17:10:57 +0100

    on heroku

--- app/assets/javascripts/apiaries.js
+++ app/assets/javascripts/apiaries.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
     $('#apiaries').dataTable();
+    function latitude()
+    {
+        var latitude = parseFloat($(".gllpLatitude").val());
+        $("#apiary_latitude").val(latitude);
+    }
+    $(".col-md-4	").on("mousemove", ".gllpLatitude", latitude);
+    function longitude()
+    {
+        var longitude = parseFloat($(".gllpLongitude").val());
+        $("#apiary_longitude").val(longitude);
+    }
+    $(".col-md-4	").on("mousemove", ".gllpLongitude", longitude);
 } );
 
